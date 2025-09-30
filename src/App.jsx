5aa5486a2587b7eda178e59be7e8609327da1e32
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-09-30 19:28:28 +0200

    inserito form con un campo input in cui inserire il titolo di un nuovo articolo del blog + css

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,52 @@ function App() {
     { title: "5 Strumenti Gratuiti che Ogni Sviluppatore Dovrebbe Conoscere" },
   ]);
 
+  const [newArticleTitle, setNewArticleTitle] = useState("");
+
+  {
+    /*funzione che gestisce l'invio del form*/
+  }
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newArticle = {
+      title: newArticleTitle,
+    };
+    {
+      /* creo un nuovo array con i vecchi articoli (...) più quello nuovo.*/
+    }
+    setArticles([...articles, newArticle]);
+
+    {
+      /*Pulisce il campo input dopo l'aggiunta*/
+    }
+    setNewArticleTitle("");
+  };
+
   return (
     <div className="container">
       <h1>My Blog</h1>
 
-      <section className="articles-section">
-        <h2>Articoli</h2>
-        <ul>
-          {/*.map per trasformare ogni oggetto nell'array "articles" in un
-          elemento */}
-          {articles.map((article, index) => (
-            <li key={index}>{article.title}</li>
-          ))}
-        </ul>
+      <section className="form-section">
+        <h2>Aggiungi un nuovo articolo</h2>
+        <form onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            placeholder="Titolo del nuovo articolo..."
+            value={newArticleTitle}
+            onChange={(e) => setNewArticleTitle(e.target.value)}
+          />
+          <button type="submit">Aggiungi</button>
+        </form>
+        <section className="articles-section">
+          <h2>Articoli:</h2>
+          <ul>
+            {/*.map per trasformare ogni oggetto nell'array "articles" in un elemento */}
+            {articles.map((article, index) => (
+              <li key={index}>{article.title}</li>
+            ))}
+          </ul>
+        </section>
       </section>
     </div>
   );
